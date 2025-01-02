8b93120c05a784c9a6282d25f0e80e8b0f51ef49
shi0rik0 <anguuan@outlook.com>
2025-01-02 16:14:43 +0800

    implement static interface

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,49 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ChatBubble from '@/components/ChatBubble.vue'
 </script>
 
 <template>
-  <div></div>
+  <div class="top-bar flex justify-between items-center fixed top-0 left-0 right-0 h-16 z-50">
+    <div class="ml-6">
+      <img src="@/assets/back.svg" />
+    </div>
+    <div>
+      <span>相亲相爱一家人（5）</span>
+    </div>
+    <div class="mr-6">
+      <img src="@/assets/account.svg" />
+    </div>
+  </div>
+  <div class="flex flex-col h-screen">
+    <div class="main-content flex-1 overflow-y-scroll hide-scrollbar">
+      <div class="h-16"></div>
+      <div>
+        <ChatBubble avatar="https://wechat.xbox.work/logo.jpg">123</ChatBubble>
+        <ChatBubble avatar="https://wechat.xbox.work/logo.jpg" right>123</ChatBubble>
+      </div>
+    </div>
+    <div class="bottom-bar flex items-center h-16">
+      <img src="@/assets/speak.svg" class="mx-2" />
+      <input type="text"
+        class="flex-1 m-0 w-full h-9 px-2 rounded bg-white outline-none border-0 shadow-none caret-green-500 text-base font-normal" />
+      <img src="@/assets/emoji.svg" class="ml-2" />
+      <img src="@/assets/extra.svg" class="mx-2" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.top-bar {
+  background-color: rgba(237, 237, 237, 0.8);
+  border-bottom: 1px solid rgb(220, 223, 213);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.main-content {
+  background-color: rgba(237, 237, 237);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.bottom-bar {
+  background-color: rgba(237, 237, 237);
+  border-top: 1px solid rgb(220, 223, 213);
 }
 </style>
---
+++ src/components/ChatBubble.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { right = false, avatar } = defineProps<{
+  right?: boolean,
+  avatar: string,
+}>()
+</script>
+
+<template>
+  <div v-if="!right" class="flex items-center w-full h-20">
+    <img :src="avatar" class="rounded w-10 h-10 overflow-clip mx-4" />
+    <div class="w-2 h-2 bg-white rotate-45 translate-x-[50%]"></div>
+    <div class="bg-white rounded h-10 p-2 flex items-center">
+      <slot></slot>
+    </div>
+  </div>
+  <div v-else class="flex justify-end items-center w-full h-20">
+    <div class="bg-[#a9ea7a] rounded h-10 p-2 flex items-center">
+      <slot></slot>
+    </div>
+    <div class="w-2 h-2 bg-[#a9ea7a] rotate-45 translate-x-[-50%]"></div>
+    <img :src="avatar" class="rounded w-10 h-10 overflow-clip mx-4" />
+  </div>
+</template>
+
+<style scoped></style>
