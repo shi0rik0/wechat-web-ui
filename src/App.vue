467f91f38fdf8878e35d7c838910a27e63b29039
shi0rik0 <anguuan@outlook.com>
2025-01-06 14:49:10 +0800

    add echo function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import ChatBubble from '@/components/ChatBubble.vue'
+
+import { ref } from 'vue'
+
+type Message = {
+  content: string
+  my: boolean
+}
+
+const messages = ref<Message[]>(
+  [
+    { content: '你好，我是一台复读机。', my: false },
+  ]
+)
+
+const inputValue = ref<string>('')
+
+function handleSend() {
+  messages.value.push({ content: inputValue.value, my: true })
+  const v = inputValue.value
+  setTimeout(() => {
+    messages.value.push({ content: `复读中：${v}`, my: false })
+  }, 1000)
+  inputValue.value = ''
+}
+
 </script>
 
 <template>
@@ -18,13 +43,14 @@ import ChatBubble from '@/components/ChatBubble.vue'
     <div class="main-content flex-1 overflow-y-scroll hide-scrollbar">
       <div class="h-16"></div>
       <div>
-        <ChatBubble avatar="https://wechat.xbox.work/logo.jpg">123</ChatBubble>
-        <ChatBubble avatar="https://wechat.xbox.work/logo.jpg" right>123</ChatBubble>
+        <ChatBubble v-for="(v, i) in messages" avatar="https://wechat.xbox.work/logo.jpg" :right="v.my" :key="i">
+          {{ v.content }}
+        </ChatBubble>
       </div>
     </div>
     <div class="bottom-bar flex items-center h-16">
       <img src="@/assets/speak.svg" class="mx-2" />
-      <input type="text"
+      <input type="text" v-model="inputValue" @keyup.enter="handleSend"
         class="flex-1 m-0 w-full h-9 px-2 rounded bg-white outline-none border-0 shadow-none caret-green-500 text-base font-normal" />
       <img src="@/assets/emoji.svg" class="ml-2" />
       <img src="@/assets/extra.svg" class="mx-2" />
--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -6,18 +6,18 @@ const { right = false, avatar } = defineProps<{
 </script>
 
 <template>
-  <div v-if="!right" class="flex items-center w-full h-20">
+  <div v-if="!right" class="flex w-full my-4">
     <img :src="avatar" class="rounded w-10 h-10 overflow-clip mx-4" />
-    <div class="w-2 h-2 bg-white rotate-45 translate-x-[50%]"></div>
-    <div class="bg-white rounded h-10 p-2 flex items-center">
+    <div class="w-2 h-2 mt-4 bg-white rotate-45 translate-x-[50%]"></div>
+    <div class="bg-white rounded p-2 flex items-center max-w-[60%]">
       <slot></slot>
     </div>
   </div>
-  <div v-else class="flex justify-end items-center w-full h-20">
-    <div class="bg-[#a9ea7a] rounded h-10 p-2 flex items-center">
+  <div v-else class="flex justify-end w-full my-4">
+    <div class="bg-[#a9ea7a] rounded p-2 flex items-center max-w-[60%]">
       <slot></slot>
     </div>
-    <div class="w-2 h-2 bg-[#a9ea7a] rotate-45 translate-x-[-50%]"></div>
+    <div class="w-2 h-2 mt-4 bg-[#a9ea7a] rotate-45 translate-x-[-50%]"></div>
     <img :src="avatar" class="rounded w-10 h-10 overflow-clip mx-4" />
   </div>
 </template>
